c470b4a40bc029f0495415ccdbed0081a5fbc5f2
Christopher Carter <christopher.carter.777@gmail.com>
2016-05-14 19:00:56 -0700

    Initial commit-working

---
+++ src/Step46Drill/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+
+namespace Step46Drill
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            //what time is it
+            DateTime now = DateTime.Now;
+            DateTime since = now.AddDays(-1);
+
+            //set paths
+            string desktop = @"C:\Users\Chris\Desktop\";
+            string workFolder = desktop + @"Working\";
+            string backupFolder = desktop + @"Backup\";
+            string logFile = desktop + @"Backup\ log.txt";
+            DirectoryInfo workDir = new DirectoryInfo(workFolder);
+
+            //generate a list of recently modified or created files
+            List<FileInfo> filesToBackup = BuildBackupList(workDir, since);
+
+            //copy each new file to the backup directory
+            foreach (FileInfo fileToBackup in filesToBackup)
+            {
+                CopyFile(fileToBackup, backupFolder);
+            }
+        //end Main method
+        }
+
+        public static List<FileInfo> BuildBackupList(DirectoryInfo dir, DateTime since)
+        {
+            Console.WriteLine("Building list of files to backup since " + since + ":");
+            List<FileInfo> fileList = new List<FileInfo>();
+            FileInfo[] files = dir.GetFiles();
+
+            foreach (FileInfo f in files)
+            {
+                Console.WriteLine(f.Name + " created: " + f.CreationTime + "\tlast written: " + f.LastWriteTime + " compared with " + since);
+                if (f.CreationTime <= since || f.LastWriteTime <= since)
+                {
+                    Console.WriteLine(" Added ");
+                    fileList.Add(f);
+                }
+                if (true)
+                {
+                    Console.WriteLine(" Added anyway.");
+                    fileList.Add(f);
+                }
+            }
+            return fileList;
+        }
+
+        public static void CopyFile (FileInfo fileToBackup, string backupFolder)
+        {
+            string destination = backupFolder + fileToBackup.Name;
+            fileToBackup.CopyTo(destination, true);
+            Console.WriteLine("Copying " + fileToBackup.Name + " to " + destination);
+        }
+    //end Program class
+    }
+}
---
+++ src/Step46Drill/Properties/AssemblyInfo.cs
@@ -0,0 +1,23 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Step46Drill")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Step46Drill")]
+[assembly: AssemblyCopyright("Copyright ©  2016")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("99f68048-0a5c-43db-9d82-ff12d0b7df8d")]
